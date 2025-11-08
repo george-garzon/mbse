c1316a4ae3b4750fa8560278c43bf16f773465fa
George Garzon Mac <george.garzon@olympiapharmacy.com>
2025-11-07 22:28:11 -0500

    init 7

--- mbse-chat/app/components/Footer.tsx
+++ mbse-chat/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer
-            className="flex flex-wrap gap-3.5 w-full mb-10 items-center justify-center text-sm text-slate-300 z-[1]">
+            className="flex flex-wrap gap-3.5 w-full mt-10 items-center justify-center text-sm text-slate-300 z-[1]">
             <a href="https://www.arcfield.com/privacy-policy" target="_blank"
                className="hover:textPrimary transition-theme">
                 Privacy Policy and Terms of Use
